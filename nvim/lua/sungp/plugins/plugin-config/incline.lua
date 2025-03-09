6163f9ff4c35081c737daee55894d6dce8103ec6
sung2708 <aki18082002@gmail.com>
2025-03-10 00:19:55 +0700

    config incline

--- nvim/lua/sungp/plugins/plugin-config/incline.lua
+++ nvim/lua/sungp/plugins/plugin-config/incline.lua
@@ -1,59 +1,48 @@
-return{
+return {
   'b0o/incline.nvim',
   config = function()
     local devicons = require 'nvim-web-devicons'
-require('incline').setup {
-  render = function(props)
-    local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
-    if filename == '' then
-      filename = '[No Name]'
-    end
-    local ft_icon, ft_color = devicons.get_icon_color(filename)
+    require('incline').setup {
+      render = function(props)
+        local buf = props.buf
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ':t')
+        filename = filename ~= '' and filename or '[No Name]'
+        local ft_icon, ft_color = devicons.get_icon_color(filename)
 
-    local function get_git_diff()
-      local icons = { removed = '', changed = '', added = '' }
-      local signs = vim.b[props.buf].gitsigns_status_dict
-      local labels = {}
-      if signs == nil then
-        return labels
-      end
-      for name, icon in pairs(icons) do
-        if tonumber(signs[name]) and signs[name] > 0 then
-          table.insert(labels, { icon .. signs[name] .. ' ', group = 'Diff' .. name })
+        local function get_git_diff()
+          local signs = vim.b[buf].gitsigns_status_dict
+          if not signs then return {} end
+          local icons = { added = '  ', changed = '  ', removed = '  ' }
+          local labels = {}
+          for name, icon in pairs(icons) do
+            if signs[name] and signs[name] > 0 then
+              table.insert(labels, { icon .. signs[name] .. ' ', group = 'Diff' .. name })
+            end
+          end
+          return labels
         end
-      end
-      if #labels > 0 then
-        table.insert(labels, { '┊ ' })
-      end
-      return labels
-    end
 
-    local function get_diagnostic_label()
-      local icons = { error = '', warn = '', info = '', hint = '' }
-      local label = {}
-
-      for severity, icon in pairs(icons) do
-        local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
-        if n > 0 then
-          table.insert(label, { icon .. n .. ' ', group = 'DiagnosticSign' .. severity })
+        local function get_diagnostics()
+          local icons = { error = '  ', warn = '  ', info = '   ', hint = '  ' }
+          local labels = {}
+          for severity, icon in pairs(icons) do
+            local count = #vim.diagnostic.get(buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+            if count > 0 then
+              table.insert(labels, { icon .. count .. ' ', group = 'DiagnosticSign' .. severity })
+            end
+          end
+          return labels
         end
-      end
-      if #label > 0 then
-        table.insert(label, { '┊ ' })
-      end
-      return label
-    end
 
-    return {
-      { get_diagnostic_label() },
-      { get_git_diff() },
-      { (ft_icon or '') .. ' ', guifg = ft_color, guibg = 'none' },
-      { filename .. ' ', gui = vim.bo[props.buf].modified and 'bold,italic' or 'bold' },
-      { '┊  ' .. vim.api.nvim_win_get_number(props.win), group = 'DevIconWindows' },
+        return {
+          { get_diagnostics() },
+          { get_git_diff() },
+          { (ft_icon or '') .. ' ', guifg = ft_color },
+          { filename .. ' ', gui = vim.bo[buf].modified and 'bold,italic' or 'bold' },
+          { '   ' .. vim.api.nvim_win_get_number(props.win), group = 'DevIconWindows' },
+        }
+      end,
     }
   end,
-}
-  end,
-  -- Optional: Lazy load Incline
   event = 'VeryLazy',
 }
